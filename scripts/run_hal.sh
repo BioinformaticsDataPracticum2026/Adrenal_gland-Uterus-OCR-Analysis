c8ea4043fb7fe4831b0ef9b9f517708274ab370a
xinyili3 <xinyili3@andrew.cmu.edu>
2026-03-28 00:11:40 -0400

    add: hal

--- scripts/run_hal.sh
+++ scripts/run_hal.sh
@@ -1,38 +1,3 @@
-# Adrenal_gland-Uterus-OCR-Analysis
-
-03-713: Bioinformatics Data Integration Practicum, Spring 2026
-
-Team members: Xinyi L., Jimmy L., Yu Hsin C.
-
-## Project Overview
-
-todo
-
-## Evaluate data quality
-
-
-
-## Map open chromatin regions across species
-
-Multi-species alignment: `/ocean/projects/bio230007p/ikaplow/Alignments/10plusway-master.hal`
-
-ATAC-seq:
-
-```bash
-# Human
-/ocean/projects/bio230007p/xli51/data/idr_Conservative_Peaks/Human_AdrenalGland_idr.conservative_peak.narrowPeak
-/ocean/projects/bio230007p/xli51/data/idr_Conservative_Peaks/Human_Uterus_idr.conservative_peak.narrowPeak
-
-# Mouse
-/ocean/projects/bio230007p/xli51/data/idr_Conservative_Peaks/Mouse_AdrenalGland_idr.conservative_peak.narrowPeak
-/ocean/projects/bio230007p/xli51/data/idr_Conservative_Peaks/Mouse_Uterus_idr.conservative_peak.narrowPeak
-```
-
-Using https://github.com/pfenninglab/halLiftover-postprocessing/tree/master
-
-Set -t (1h not enough)
-
-```bash
 #!/bin/bash
 
 mkdir -p logs
@@ -73,19 +38,5 @@ sbatch -p RM-shared --mem=2000 -t 08:00:00 $HALPER_SCRIPT \
   -s Mouse \
   -t Human \
   -c $HAL
-```
 
-## Compare open chromatin between species
-
-
-
-## Find biological processes that are likely to be regulated by open chromatin regions
-
-
-
-## Compare candidate enhancers to candidate promoters
-
-
-
-## Find transcription factors that tend to bind open chromatin regions
-
+echo "All jobs submitted."
